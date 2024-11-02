09ed2c9d0c437ee7ead8580c9e839b4d5f4cb565
Mark Tedra <marktedra29@gmail.com>
2024-11-02 17:14:24 +0800

    Implementing the Cities List

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
+/* eslint-disable no-unused-vars */
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Hommepage from "./pages/Homepage";
 import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+
+const URL = "http://localhost:9000";
 
 function App() {
+  const [cities, setCieties] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        const res = await fetch(`${URL}/cities`);
+        const data = await res.json();
+        setCieties(data);
+      } catch {
+        alert("There was an error Loading data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <div>
       <BrowserRouter>
@@ -16,7 +39,11 @@ function App() {
           <Route path="pricing" element={<Pricing />} />
           <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />}>
-            <Route path="cities" element={<p>List of Cities</p>} />
+            <Route
+              index
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            />
+            <Route path="cities" element={<CityList />} />
             <Route path="countries" element={<p>Countries</p>} />
             <Route path="form" element={<p>Form</p>} />
           </Route>
